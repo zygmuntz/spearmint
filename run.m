aeb15bbab34386e9da55402ed462f497bc267257
zygmuntz <zajac.zygmunt@gmail.com>
2012-12-21 22:51:56 +0100

    .

--- run.m
+++ run.m
@@ -1,11 +1,15 @@
 args = argv();
 
+# we need numbers
 for i = 1:nargin
 	args{i} = str2num( args{i} );
 end
 
-args
-
+# init the neural network
 a4_init
-#a4_main( 300, args{1}, args{2}, args{3}, args{4} )
+
+# if we were optimizing a few params
+# a4_main( 300, args{1}, args{2}, args{3}, args{4} )
+
+# learn and validate
 a4_main( 300, 0.02, args{1}, 1000 )
